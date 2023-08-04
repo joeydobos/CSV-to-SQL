0ae5d134a70ea88763d065a67b956c4d29c1cfe9
joeydobos <126507263+joeydobos@users.noreply.github.com>
2023-08-04 14:52:45 +0100

    Update CSV_To_SQL.py

--- CSV_To_SQL.py
+++ CSV_To_SQL.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
-
-file_name = ""
+file_name = "/Users/josephdobos/Desktop/CV's/venue_data.xlsx"
 df = pd.read_excel(file_name)
 sql = ""
 
 for _, row in df.iterrows():
+    formatted_values = []
+    for value in row:
+        if isinstance(value, int):  # Check if the value is an integer
+            formatted_values.append(str(value).strip())
+        else:
+            formatted_values.append("'{}'".format(str(value).strip()) if str(value).strip() != 'nan' else "''")
 
-    formatted_values = ["'{}'".format(str(value).strip()) if str(value).strip() != 'nan' else "''" for value in row]
-    
     sql += "INSERT INTO venues (`{}`) VALUES ({});\n".format("`, `".join(df.columns), ", ".join(formatted_values))
 
 print(sql)
